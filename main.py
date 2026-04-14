3417e77baa53f3a43a92dffed8e37870a1067152
Robin Škába <robin.skaba@volny.cz>
2026-04-14 14:07:52 +0200

    feat: removing content creator roles from inactive creators

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import logging
+
 from src.app.discord_bot import CatastrophiaBot
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="{asctime} {levelname:<8} - {name}: {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{",
+    )
+
     print("CatastrophiaBot is booting up...")
     bot = CatastrophiaBot()
     bot.launch()
