47da2bf91074248cc05fa6e5af23fd16d98761c4
Robin Škába <robin.skaba@volny.cz>
2026-04-29 13:43:38 +0200

    fix: ignore discord 500 logs

---
+++ src/config/logging_filters.py
@@ -0,0 +1,17 @@
+import logging
+
+_IGNORED_500_MESSAGES = ["WSServerHandshakeError: 520"]
+
+
+class IgnoreDiscord500Filter(logging.Filter):
+    def filter(self, record):
+        for ignored_msg in _IGNORED_500_MESSAGES:
+            if (ignored_msg in record.getMessage()) or (
+                record.exc_info and ignored_msg in str(record.exc_info[1])
+            ):
+                return False
+        return True
+
+
+logging.getLogger("discord").addFilter(IgnoreDiscord500Filter())
+logging.getLogger("discord.gateway").addFilter(IgnoreDiscord500Filter())
