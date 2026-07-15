5c2be3e9d0b4de0d2807ad8ecb594c800f2d611d
Robin Škába <robin.skaba@volny.cz>
2026-07-15 16:20:33 +0200

    feat: auto translation of ru to en

--- src/features/translation/http/translation_http.py
+++ src/features/translation/http/translation_http.py
@@ -1,12 +1,15 @@
 from enum import Enum
+import logging
 
 import aiohttp
 
 from src.common.config.config import Env
 
+_logger = logging.getLogger(__name__)
+
 
 class Language(Enum):
-    ENGLISH = ("en",)
+    ENGLISH = "en"
     RUSSIAN = "ru"
 
 
@@ -17,6 +20,9 @@ async def translate(message: str, source: Language, target: Language) -> str | N
             f"{Env.TRANSLATION_ENDPOINT}/translate", json=payload
         ) as response:
             if response.status != 200:
+                _logger.debug(
+                    f"translation of '{message}' resulted in {response.status}"
+                )
                 return None
 
             data = await response.json()
--- src/features/translation/translation_cog.py
+++ src/features/translation/translation_cog.py
@@ -1,7 +1,12 @@
-from discord import Interaction, Message, Object, app_commands
+import logging
+import re
+
+from discord import Interaction, Message, Object, TextChannel, app_commands
 from discord.ext import commands
 from src.common.config.config import Env
 from src.features.translation.http.translation_http import Language, translate
+
+_logger = logging.getLogger(__name__)
 
 
 class TranslationCog(commands.Cog):
@@ -18,8 +23,9 @@ class TranslationCog(commands.Cog):
     async def cog_unload(self):
         self._bot.tree.remove_command(self._ctx_menu.name, type=self._ctx_menu.type)
 
+    # context command for translating to russian
     async def translate_to_russian(self, interaction: Interaction, message: Message):
-        await interaction.response.defer(ephemeral=True)
+        await interaction.response.defer(ephemeral=False)
         russian_translation = await translate(
             message.content, Language.ENGLISH, Language.RUSSIAN
         )
@@ -27,6 +33,40 @@ class TranslationCog(commands.Cog):
             russian_translation if russian_translation else "Перевод сейчас недоступен."
         )  # "Translation is unavailable right now."
         await interaction.followup.send(response)
+
+    async def get_or_create_webhook(self, channel: TextChannel):
+        webhooks = await channel.webhooks()
+        for webhook in webhooks:
+            if webhook.user == self._bot.user:
+                return webhook
+        return await channel.create_webhook(name="TranslationWebhook")
+
+    @commands.Cog.listener()
+    async def on_message(self, message: Message):
+        # do not retrigger on bot messages or empty
+        if message.author.bot or not message.content:
+            return
+
+        # regex search for basic cyrillic characters
+        if not re.search(r"[А-Яа-яЁё]", message.content):
+            return
+
+        translation = await translate(
+            message.content, Language.RUSSIAN, Language.ENGLISH
+        )
+        if not translation:
+            _logger.warning(f"could not translate '{message.content}' to english")
+            return
+
+        # update message with translation
+        webhook = await self.get_or_create_webhook(message.channel)
+        msg_with_translation = f"{message.content} *({translation})*"
+        await webhook.send(
+            content=msg_with_translation,
+            username=message.author.display_name,
+            avatar_url=message.author.display_avatar.url,
+        )
+        await message.delete()
 
 
 async def setup(bot: commands.Bot):
