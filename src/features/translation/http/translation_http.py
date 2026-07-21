8dd2c5a399d5b296a2156658dadf0c1ec4e83a00
Robin Škába <robin.skaba@volny.cz>
2026-07-21 12:34:02 +0200

    fix: clienterror handling

--- src/features/translation/http/translation_http.py
+++ src/features/translation/http/translation_http.py
@@ -16,15 +16,19 @@ class Language(Enum):
 async def translate(message: str, source: Language, target: Language) -> str | None:
     async with aiohttp.ClientSession() as session:
         payload = {"q": message, "source": source.value, "target": target.value}
-        async with session.post(
-            f"{Env.TRANSLATION_ENDPOINT}/translate", json=payload
-        ) as response:
-            if response.status != 200:
-                _logger.debug(
-                    f"translation of '{message}' resulted in {response.status}"
-                )
-                return None
+        try:
+            async with session.post(
+                f"{Env.TRANSLATION_ENDPOINT}/translate", json=payload
+            ) as response:
+                if response.status != 200:
+                    _logger.debug(
+                        f"translation of '{message}' resulted in {response.status}"
+                    )
+                    return None
 
-            data = await response.json()
-            translation = data.get("translatedText", "translation failed")
-            return translation
+                data = await response.json()
+                translation = data.get("translatedText", "translation failed")
+                return translation
+        except aiohttp.ClientError as e:
+            _logger.debug(f"translation request failed due to: {e}")
+            return None
